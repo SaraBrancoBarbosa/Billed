f4dccf2d2074d280ce8eddc0e45719abe209b191
SaraBrancoBarbosa <165152164+SaraBrancoBarbosa@users.noreply.github.com>
2024-12-05 16:03:56 +0100

    Better readability of the code (+ keeping the old date format in the comments)

--- Billed-app-FR-Front-main/src/app/format.js
+++ Billed-app-FR-Front-main/src/app/format.js
@@ -1,3 +1,4 @@
+
 export const formatDate = (dateStr) => {
   const date = new Date(dateStr)
   const year = date.getFullYear()
@@ -5,6 +6,17 @@ export const formatDate = (dateStr) => {
   const day = String(date.getDate()).padStart(2, '0')
   return `${year}-${month}-${day}`
 }
+
+/*
+export const formatDate = (dateStr) => {
+  const date = new Date(dateStr)
+  const ye = new Intl.DateTimeFormat('fr', { year: 'numeric' }).format(date)
+  const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
+  const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
+  const month = mo.charAt(0).toUpperCase() + mo.slice(1)
+  return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
+}
+*/
  
 export const formatStatus = (status) => {
   switch (status) {
@@ -13,6 +25,6 @@ export const formatStatus = (status) => {
     case "accepted":
       return "Accepté"
     case "refused":
-      return "Refused"
+      return "Refusé"
   }
 }
--- Billed-app-FR-Front-main/src/views/BillsUI.js
+++ Billed-app-FR-Front-main/src/views/BillsUI.js
@@ -21,16 +21,16 @@ const row = (bill) => {
 
 const rows = (data) => {
   // To order bills from earliest to latest
-  if (data){
-    data.sort((a, b) => new Date(b.date) - new Date(a.date))
-  }
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  if (!data || data.length < 1) return ""
+  data.sort((a, b) => new Date(b.date) - new Date(a.date))
+  
+  return data.map(bill => row(bill)).join("")
 }
 
 export default ({ data: bills, loading, error }) => {
   
   const modal = () => (`
-    <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+    <div class="modal fade" id="modaleFile" data-testid="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
           <div class="modal-header">
